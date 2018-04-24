645d363e467bc5c2e166e53132ac3395759eaad1
Lukasz Mitura <lukasz.mitura@hexagon.com>
2018-04-24 16:24:04 +0200

    Refactored main module

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ from enums import Weekdays, TimeUnits, PlanTypes
 from SqliteExpensesPersister import SqliteExpensesPersister
 from Expense import Expense
 
-database_path = ".\dbs\expenses-planner.db"
+DATABASE_PATH = ".\dbs\expenses-planner.db"
 
-expenses_persister = SqliteExpensesPersister(database_path)
+def main():
 
-fake_expense = Expense(
-    uuid.uuid4(),
-    "Another Cell Phone",
-    1499.94,
-    "2018-04-23",
-    uuid.uuid4()
-)
+    expenses_persister = SqliteExpensesPersister(DATABASE_PATH)
 
-expenses_persister.add_expense(fake_expense)
+    fake_expense = Expense(
+        uuid.uuid4(),
+        "Another Cell Phone",
+        1499.94,
+        "2018-04-23",
+        uuid.uuid4()
+    )
+
+    expenses_persister.add_expense(fake_expense)
+
+main()
