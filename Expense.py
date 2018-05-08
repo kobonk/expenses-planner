560b7535ea0208594d41fdcf8aaa26ff5d5bfca6
Lukasz Mitura <lukasz.mitura@hexagon.com>
2018-05-08 12:15:32 +0200

    Added missing method comments to Expense class

--- Expense.py
+++ Expense.py
@@ -1,5 +1,6 @@
 """The module contains Expense class"""
 import time
+
 class Expense:
     """The class is a model for a single planned Expense"""
 
@@ -12,27 +13,35 @@ class Expense:
         self.__done = done
 
     def get_expense_id(self):
+        """Returns the id of the Expense"""
         return self.__expense_id
 
     def get_name(self):
+        """Returns the name of the Expense"""
         return self.__name
 
     def get_cost(self):
+        """Returns the cost of the Expense"""
         return self.__cost
 
     def get_deadline(self):
+        """Returns the deadline (seconds since epoch) of the Expense"""
         return self.__deadline
 
     def get_plan_id(self):
+        """Returns the id of the plan applied to the Expense"""
         return self.__plan_id
 
     def is_done(self):
+        """Returns true if the planned Expense has been paid"""
         return self.__done
 
     def create_builder(self):
+        """Creates an instance of a Builder object"""
         return Builder(self)
 
     def get_deadline_string(self):
+        """Returns the deadline in form of user-friendly string"""
         return time.ctime(self.__deadline)
 
     def to_string(self):
@@ -60,9 +69,11 @@ class Builder:
         )
 
     def build(self):
+        """Returns the built instance of Expense"""
         return self.__expense
 
     def mark_as_done(self):
+        """Marks the Expense as done (paid)"""
         self.__expense = Expense(
             self.__expense.get_expense_id(),
             self.__expense.get_name(),
@@ -74,6 +85,7 @@ class Builder:
         return self
 
     def mark_as_not_done(self):
+        """Marks the Expense as not done (not paid)"""
         self.__expense = Expense(
             self.__expense.get_expense_id(),
             self.__expense.get_name(),
