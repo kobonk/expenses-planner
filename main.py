c9a2a16566651abdcf6a161d67ebcabfdad8bc25
Lukasz Mitura <lukasz.mitura@hexagon.com>
2018-06-08 16:35:51 +0200

    Removed obsolete dependencies

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 """This is the main starting point of the application"""
-import http.server
 import os
-import re
-import sqlite3
-import time
-import uuid
 
-from src.enums import Weekdays, TimeUnits, PlanTypes
-from src.Expense import Expense
 from src.renderer.TexttableExpensesRenderer import TexttableExpensesRenderer
 from src.SqliteExpensesRetriever import SqliteExpensesRetriever
 
@@ -17,7 +10,7 @@ EXPENSES_TABLE_NAME = "expenses"
 
 def main():
     """Main application entry point"""
-    
+
     expenses_retriever = SqliteExpensesRetriever(DATABASE_PATH, EXPENSES_TABLE_NAME)
     expenses = expenses_retriever.retrieve_unpaid_expenses()
     renderer = TexttableExpensesRenderer()
